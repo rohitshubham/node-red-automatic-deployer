f449d36d36a14efe9ba2dfdfd7f7111b851ba9d1
Rohit <rohit.raj@aalto.fi>
2020-06-05 09:52:51 +0000

    Added ability to read all the json files in the directory

--- deploy_flow/app.py
+++ deploy_flow/app.py
@@ -1,15 +1,25 @@
 import requests
 import json
 import sys
+import os
+
 sys.stdout.flush()
 
+total_flows = 0
+path_to_jsonfiles = "flows/"
+url = 'http://node-red:1880/flows'
+
 def load_file():
-    data = {}
-    with open('flow/temp.json') as json_file:
-        data = json.load(json_file)
+    data = []
+    for file in os.listdir(path_to_jsonfiles):
+        full_filename = "%s/%s" % (path_to_jsonfiles, file)
+        with open(full_filename, 'r') as json_file:
+            data.append(json.load(json_file))
     return data
 
+
 print("Loaded file")
-url = 'http://node-red:1880/flows'
-response = requests.post(url, json=load_file())
+data = load_file()
+for json_body in data:
+    response = requests.post(url, json=json_body)
 print("Completed Flows")
